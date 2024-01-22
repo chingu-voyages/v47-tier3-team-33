b323d82e920f400ab431a414c6cfabd9e41cf7e4
Tishtanya Shaw <tishtanyaa@icloud.com>
2024-01-22 00:32:14 -0500

    Update footer imports

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -3,44 +3,45 @@ import { FaGithub } from "react-icons/fa";
 import { FaLinkedinIn } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
+import LOGO from '../assets/omni-logo.png';
 
 function Footer() {
     return (
-        <footer className="bg-pink text-white w-full h-[355px] relative">
+        <footer className="text-white w-full md:h-[355px] relative">
             <div className='absolute top-0 left-0 w-full overflow-hidden bg-pink'>
                 <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
                 <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="relative block fill-white"></path>
                 </svg>
             
-            <div className='w-full mt-6 flex flex-col md:flex-row justify-center items-center md:justify-around'>
-                <div className='mb-6 md:mb-0 bg-yellow'><div className="">LOGO</div></div>
-                <div className="grid md:grid-cols-3 grid-cols-1 px-6 text-center md:text-left">
+            <div className='w-full mt-6 flex flex-col md:flex-row justify-center items-center md:justify-around -pt-20 pb-4 md:pb-0'>
+                <div className='-mt-16 md:mt-0'><img src={LOGO} alt="logo" /></div>
+                <div className="grid md:grid-cols-3 grid-cols-1 px-6 text-center md:text-left -mt-24 md:mt-12">
                     <div className='flex flex-col gap-5 mb-4 md:mb-0'>
-                        <h2 className="text-teal text-md">Navigation</h2>
+                        <h2 className="text-teal text-xl">Navigation</h2>
                         <ul className='space-y-2'>
-                            <li className='cursor-pointer hover:text-yellow'>About</li>
-                            <li className='cursor-pointer hover:text-yellow'>Events</li>
-                            <li className='cursor-pointer hover:text-yellow'>Categories</li>
+                            <li className='text-lg cursor-pointer hover:text-yellow'>About</li>
+                            <li className='text-lg cursor-pointer hover:text-yellow'>Events</li>
+                            <li className='text-lg cursor-pointer hover:text-yellow'>Categories</li>
                         </ul>
                     </div>
                     <div className='flex flex-col gap-5 mb-4 md:mb-0'>
-                       <h2 className="text-teal text-lg">Company</h2>
+                       <h2 className="text-teal text-xl">Company</h2>
                         <ul className='space-y-2'>
-                            <li className='cursor-pointer hover:text-yellow'>Developers</li>
-                            <li className='cursor-pointer hover:text-yellow'>Acessibility</li>
-                            <li className='cursor-pointer hover:text-yellow'>Terms of Service</li>
+                            <li className='text-lg cursor-pointer hover:text-yellow'>Developers</li>
+                            <li className='text-lg cursor-pointer hover:text-yellow'>Acessibility</li>
+                            <li className='text-lg cursor-pointer hover:text-yellow'>Terms of Service</li>
                         </ul>
                     </div>
                     <div className='flex flex-col gap-5 mb-4 md:mb-0'>
-                        <h2 className="text-teal text-lg">Contact Us</h2>
+                        <h2 className="text-teal text-xl">Contact Us</h2>
                          <ul className='space-y-2'>
-                            <li className='cursor-pointer hover:text-yellow'>Email: company@omnievents.com</li>
-                            <li className='cursor-pointer hover:text-yellow'>Phone: 443-678-4567</li>
+                            <li className='text-lg cursor-pointer hover:text-yellow'>Email: company@omnievents.com</li>
+                            <li className='text-lg cursor-pointer hover:text-yellow'>Phone: 443-678-4567</li>
                             <li className='flex items-center justify-center md:justify-start space-x-3 cursor-pointer'>
-                                <FaGithub className='hover:text-yellow'/>
-                                <FaLinkedinIn className='hover:text-yellow'/>
-                                <FaTwitter className='hover:text-yellow'/>
-                                <FaInstagram className='hover:text-yellow'/>
+                                <FaGithub className='hover:text-yellow text-xl'/>
+                                <FaLinkedinIn className='hover:text-yellow text-xl'/>
+                                <FaTwitter className='hover:text-yellow text-xl'/>
+                                <FaInstagram className='hover:text-yellow text-xl'/>
                             </li>
                         </ul>
                     </div>
