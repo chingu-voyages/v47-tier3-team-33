0352807049658868c1cfd95eaa76a5c8b3831ccb
Tishtanya Shaw <Tishtanyaa@icloud.com>
2024-01-25 10:39:48 -0500

    Update homepage, added svg, and added typewriter dependency

--- frontend/src/components/Features.tsx
+++ frontend/src/components/Features.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Planner from './../assets/planner-svg.svg';
 import { Link } from 'react-router-dom';
+import Typewriter from 'typewriter-effect';
+import confetti from '../assets/—Pngtree—colorful confetti falling png isolated_7432197.png';
 
 function Features() {
 	return (
@@ -15,15 +17,15 @@ function Features() {
 					</h3>
 				</div>
 				<div className='flex items-center justify-center space-x-10 mt-[80px] '>
-					<div className='border bottom-2 border-black w-[300px] h-[400px] rounded-md'></div>
-					<div className='border bottom-2 border-black w-[300px] h-[400px] rounded-md'></div>
-					<div className='border bottom-2 border-black w-[300px] h-[400px] rounded-md'></div>
+					<div className='border bottom-2 border-black w-[300px] h-[370px] rounded-md'></div>
+					<div className='border bottom-2 border-black w-[300px] h-[370px] rounded-md'></div>
+					<div className='border bottom-2 border-black w-[300px] h-[370px] rounded-md'></div>
 				</div>
 			</div>
 			<div className='bg-teal mt-36 h-[730px]'>
 				<div className='w-full h-full flex justify-center items-center'>
 					<div className='w-[50%] flex justify-center items-center'>
-						<img src={Planner} alt='planner' />
+						<img src={Planner} alt='planner' className='h-[400px]' />
 					</div>
 					<div className='w-[50%] text-center space-y-12 -mt-18'>
 						<h2 className='text-5xl text-pink'>No More Chucky Planner 🚫</h2>
@@ -31,20 +33,30 @@ function Features() {
 							Try our event planner made easy just for you
 						</p>
 						<Link to='/about'>
-							<button className='bg-pink p-4 text-2xl rounded-md mt-20'>
+							<button className='bg-pink p-4 text-2xl rounded-md mt-20 text-white'>
 								Learn more
 							</button>
 						</Link>
 					</div>
 				</div>
 			</div>
-			<div className=' h-[850px] flex items-center justify-center flex-col'>
-				<h2 className='text-pink text-6xl'>
-					Create an event for your next thing
-				</h2>
-				<button className='bg-pink p-6 text-3xl rounded-md mt-28'>
-					Create an event
-				</button>
+			<div className=' h-[850px] w-full flex items-center justify-center text-center flex-col'>
+				<img src={confetti} className=' w-full relative h-[850px]' />
+				<div className='absolute z-50 bg-white'>
+					<h2 className='text-pink text-5xl w-full'>
+						Create an event for your next{' '}
+						<Typewriter
+							options={{
+								strings: ['meetup', 'party', 'concert'],
+								autoStart: true,
+								loop: true,
+							}}
+						/>{' '}
+					</h2>
+					<button className='bg-pink p-6 text-3xl rounded-md mt-28'>
+						Create an event
+					</button>
+				</div>
 			</div>
 		</div>
 	);
