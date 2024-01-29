619f7905d7efc2664409662ebe6a86be683e8f89
Wilson Chu <wilson1.chu@gmail.com>
2024-01-28 19:42:15 -0500

    Convert .js to .ts, change ..requires to import

--- backend/controllers/userController.ts
+++ backend/controllers/userController.ts
@@ -1,29 +1,7 @@
-// import { Request, Response } from 'express';
-// import UserModel from '../models/User';
+import { Request, Response } from 'express';
+import UserModel from '../models/User';
 
-// export const createUser = async (req: Request, res: Response): Promise<void> => {
-//   try {
-//     const user = new UserModel(req.body);
-//     const savedUser = await user.save();
-//     res.json(savedUser);
-//   } catch (error) {
-//     res.status(500).json({ error: 'Internal Server Error' });
-//   }
-// };
-
-// export const getUserById = async (req: Request, res: Response): Promise<void> => {
-//   try {
-//     const user = await UserModel.findById(req.params.id);
-//     res.json(user);
-//   } catch (error) {
-//     res.status(500).json({ error: 'Internal Server Error' });
-//   }
-// };
-
-const { Request, Response } = require('express');
-const UserModel = require('../models/User');
-
-exports.createUser = async (req, res) => {
+export const createUser = async (req: Request, res: Response) => {
   try {
     const user = new UserModel(req.body);
     const savedUser = await user.save();
@@ -33,7 +11,7 @@ exports.createUser = async (req, res) => {
   }
 };
 
-exports.getUserById = async (req, res) => {
+export const getUserById = async (req: Request, res: Response) => {
   try {
     const user = await UserModel.findById(req.params.id);
     if (!user) {
