2eeba84f77abf5015e3e9bfebc2c1c5dd0266945
Tishtanya Shaw <Tishtanyaa@icloud.com>
2024-01-31 00:34:04 -0500

    Add mobile responsiveness

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -5,7 +5,7 @@ const Login = () => {
 
 	return (
 		<div className='flex flex-col items-center justify-center'>
-			<div className='w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-50 dark:border-gray-700'>
+			<div className='w-[50%] md:w-full bg-white rounded-lg shadow dark:border mt-28 md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-50 dark:border-gray-700'>
 				<div className='p-6 space-y-4 md:space-y-6 sm:p-8'>
 					<h1 className='text-center text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-gray-900'>
 						Welcome Back!
