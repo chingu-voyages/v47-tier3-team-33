69fc8f27f5b3eaa4c99188d69459e418790ec840
Tishtanya Shaw <Tishtanyaa@icloud.com>
2024-02-01 12:17:18 -0500

    Updates routes and controllers, added rsvp route and updated dependencies

--- backend/routers/User.ts
+++ backend/routers/User.ts
@@ -1,10 +1,15 @@
 import express from 'express';
-import { createUser, getUserById } from '../controllers/userController';
+import {
+	createUser,
+	getUserById,
+	loginUser,
+} from '../controllers/userController';
 
 const router = express.Router();
 
 // User Routes
 router.post('/', createUser);
+router.post('/login', loginUser);
 router.get('/:id', getUserById);
 // More for later...
 
