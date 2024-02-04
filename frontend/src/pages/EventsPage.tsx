857c611de255cbbdcb62e6f85108718163ee6c65
Dorene07 <dstmarth@gmail.com>
2024-02-04 10:33:01 -0500

    made search bar functional

--- frontend/src/pages/EventsPage.tsx
+++ frontend/src/pages/EventsPage.tsx
@@ -15,41 +15,49 @@ interface IEvent {
 
 const EventsPage = () => {
 	const [events, setEvents] = useState<IEvent[]>([]);
-
+	const [filteredEvents, setFilteredEvents] = useState<IEvent[]>([]);
+  
 	useEffect(() => {
-		const getAllEvents = async () => {
-			await axios.get('http://localhost:8000/events').then((response) => {
-				setEvents(response.data.events);
-			});
-		};
-		getAllEvents();
+	  const getAllEvents = async () => {
+		await axios.get('http://localhost:8000/events').then((response) => {
+		  setEvents(response.data.events);
+		  setFilteredEvents(response.data.events); // Initially set to all events
+		});
+	  };
+	  getAllEvents();
 	}, []);
-
+  
+	const handleSearch = (query: string) => {
+	  // Use the .filter method to filter events based on the title or other properties
+	  const filtered = events.filter((event) =>
+		event.title.toLowerCase().includes(query.toLowerCase())
+	  );
+	  setFilteredEvents(filtered);
+	};
+  
 	return (
-		<div className=' justify-center h-full m-8 mb-72'>
-			<div className='flex justify-center pt-32 text-black text-4xl '>
-				Explore Events
-			</div>
-			<div className='flex items-center pt-20 '>
-				<SearchBar
-					onSearch={function (query: string): void {
-						throw new Error('Function not implemented.');
-					}}
-				/>
-			</div>
-			<ScrollingTags />
-			<div className='h-full'>
-				<h3 className='text-black text-2xl font-medium mb-8 mt-12'>
-					Upcoming events
-				</h3>
-				<div className='w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5'>
-					{events.map((event, idx) => (
-						<EventCard key={idx} event={event} />
-					))}
-				</div>
-			</div>
-			{/* Add other components or content here */}
+	  <div className='justify-center h-full m-8 mb-72'>
+		<div className='flex justify-center pt-32 text-black text-4xl '>
+		  Explore Events
 		</div>
+		<div className='flex items-center pt-20 '>
+		  <SearchBar onSearch={handleSearch} />
+		</div>
+		<ScrollingTags />
+		<div className='h-full'>
+		  <h3 className='text-black text-2xl font-medium mb-8 mt-12'>
+			Upcoming events
+		  </h3>
+		  <div className='w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5'>
+			{filteredEvents.map((event, idx) => (
+			  <EventCard key={idx} event={event} />
+			))}
+		  </div>
+		</div>
+		{/* Add other components or content here */}
+	  </div>
 	);
-};
-export default EventsPage;
+  };
+  
+  export default EventsPage;
+  
